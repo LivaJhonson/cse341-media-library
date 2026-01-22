062ea948a6329067008ba8e81d4a10618193b53e
LivaJhonson <livajhonson69@gmail.com>
2026-01-22 18:35:19 +0300

    modified:   swagger.js 	modified:   swagger.json

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Part 1: CRUD Operations for Movies and Directors',
   },
   host: 'https://cse341-media-library.onrender.com',
-  schemes: ['http'],
+  schemes: ['https'],
 };
 
 const outputFile = './swagger.json';
